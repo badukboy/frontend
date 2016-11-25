523dcff398d805e83059597dfe3a867878cc5be9
ck <ck@CK>
2016-11-25 15:12:49 -0800

    Added Crud functionality to Menu model and Rest

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -1,11 +1,20 @@
 <?php
 
+/**
+ * Modified to use REST client to get port data from our server.
+ */
+define('REST_SERVER', 'http://backend.local:4711');  // the REST server host
+define('REST_PORT', $_SERVER['SERVER_PORT']);               // the port you are running the server on
+
 class Menu extends MY_Model {
 
 	// constructor
 	function __construct()
 	{
 		parent::__construct();
+                 $this->load->library(['curl', 'format', 'rest']);
+                 
+              
 	}
 	public function rules() {
 		$config = [        
@@ -18,4 +27,78 @@ class Menu extends MY_Model {
 		];    
 			return $config;
 	}
+        
+        public function database() {
+            $this->db->where($this->_keyField, $key);
+                  $query = $this->db->get($this->_tableName);
+                  if ($query->num_rows() < 1)
+                        return null;
+                  return $query->row();
+        }
+        public function restcall(){     
+               $this->rest->initialize(array('server' => REST_SERVER . '/maintenance'));
+               $this->rest->option(CURLOPT_PORT, REST_PORT);
+               $result = $this->rest->get('');
+                return $result;
+        }
+        
+        // Return all records as an array of objects
+        public function all()
+        {
+                $this->rest->initialize(array('server' => REST_SERVER));
+                $this->rest->option(CURLOPT_PORT, REST_PORT);
+                return $this->rest->get('/maintenance');
+        }
+        
+        // Retrieve an existing DB record as an object
+        public function get($key, $key2 = null)
+        {
+                $this->rest->initialize(array('server' => REST_SERVER));
+                $this->rest->option(CURLOPT_PORT, REST_PORT);
+                return $this->rest->get('/maintenance/item/id/' . $key);
+        }
+        
+        // Create a new data object.
+        // Only use this method if intending to create an empty record and then
+        // populate it.
+        public function create()
+        {
+            $names = ['id','name','description','price','picture','category'];
+            $object = new StdClass;
+            foreach ($names as $name)
+                $object->$name = "";
+            return $object;
+        }
+        
+        // Delete a record from the DB
+        public function delete($key, $key2 = null)
+        {
+                $this->rest->initialize(array('server' => REST_SERVER));
+                $this->rest->option(CURLOPT_PORT, REST_PORT);
+                return $this->rest->delete('/maintenance/item/id/' . $key);
+        }
+        // Determine if a key exists
+        public function exists($key, $key2 = null)
+        {
+                $this->rest->initialize(array('server' => REST_SERVER));
+                $this->rest->option(CURLOPT_PORT, REST_PORT);
+                $result = $this->rest->get('/maintenance/item/id/' . $key);
+                return ! empty($result);
+        }
+        
+        // Update a record in the DB
+        public function update($record)
+        {
+                $this->rest->initialize(array('server' => REST_SERVER));
+                $this->rest->option(CURLOPT_PORT, REST_PORT);
+                $retrieved = $this->rest->put('/maintenance/item/id/' . $record['code'], $record);
+        }
+        
+        // Add a record to the DB
+        public function add($record)
+        {
+                $this->rest->initialize(array('server' => REST_SERVER));
+                $this->rest->option(CURLOPT_PORT, REST_PORT);
+                $retrieved = $this->rest->post('/maintenance/item/id/' . $record['code'], $record);
+        }
 }
